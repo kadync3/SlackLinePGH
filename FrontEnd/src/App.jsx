dccfce0ee053ee7ff2e75de4ea79e46ddc866493
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-01 10:56:22 -0500

    working

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react'
-
-
+import LandingPage from './components/landingPage'
+import './App'
 function App() {
 
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="https://us.123rf.com/450wm/thesilhouettequeen/thesilhouettequeen2010/thesilhouettequeen201008659/158114154-slacklining-silhouette-vector.jpg?ver=6  " className="logo" alt="Vite logo" />
-        </a>
-          <img src="https://us.123rf.com/450wm/thesilhouettequeen/thesilhouettequeen2010/thesilhouettequeen201008659/158114154-slacklining-silhouette-vector.jpg?ver=6" className="logo react" alt="React logo" />
       
-      </div>
-      <h1>SlackLinePGH</h1>
+      
+        <LandingPage/>
       </div>
   )
 }
