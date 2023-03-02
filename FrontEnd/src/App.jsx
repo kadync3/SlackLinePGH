3aa9b293c8e851251a105983fd0369f28de3b148
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-02 09:16:31 -0500

    completed basic landing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
   return (
     <div className="App">
 
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="https://us.123rf.com/450wm/thesilhouettequeen/thesilhouettequeen2010/thesilhouettequeen201008659/158114154-slacklining-silhouette-vector.jpg?ver=6  " className="logo" alt="Vite logo" />
+         </a>
       
         <LandingPage/>
 
