c6bcb00cfdcc2a4ba75570d9399f13d340cce428
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-02 10:12:53 -0500

    added title overlay

--- FrontEnd/src/components/landingPage.jsx
+++ FrontEnd/src/components/landingPage.jsx
@@ -9,8 +9,9 @@ function LandingPage (){
 <div className='landing'>
     
         <Card className='intro'>
-        <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi012vWZuT0O8NgOjoucvuPbG5R5iFw4CBCq1gNW8aZA&usqp=CAU&ec=48600113" />
+        <Card.Img variant="top"  className="card-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxwZYhl79BklqZ47ENMW_n73UtWXRhZVnHQHvkXXEzpA&usqp=CAU&ec=48600113" />
         <Card.Body>
+          <Card.Title className='card-img-overlay'>Slackline PGH</Card.Title>
           <Card.Text>
            Slacklining is a recreational activity in which a person stands or walks on a suspended length of flat webbing that is tensioned between two anchors. Slacklines are typically anchored between two trees, or two posts, or two boulders, and the line is tensioned using various techniques to create a dynamic and often bouncy surface to balance on. Slacklining can be used for balance training, tricklining , yoga, or for general physical fitness and recreation.
           </Card.Text>
