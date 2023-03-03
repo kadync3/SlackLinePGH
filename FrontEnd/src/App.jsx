da7b577d66c654c5fc41c17ba06c51431ced9821
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-03 11:18:40 -0500

    landing page loads on inital render

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PGHnavbar from './components/navbar';
-import LandingPage from './components/landingPage'
-import './App'
+import LandingPage from './components/landingPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -14,12 +13,12 @@ function App() {
     <PGHnavbar/>
             
       <Routes>
-        <Route path="/home" element={
+        <Route exact path="/" element={
             <LandingPage/>}/>
         
       </Routes>
     </BrowserRouter>
   )
-}
+};
 
-export default App
+export default App;
