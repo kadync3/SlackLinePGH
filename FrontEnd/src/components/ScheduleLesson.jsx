612299aade36086b4fae15c5a70af248e5a370ce
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-03-06 11:36:19 -0600

    converted input to card

--- FrontEnd/src/components/ScheduleLesson.jsx
+++ FrontEnd/src/components/ScheduleLesson.jsx
@@ -4,20 +4,32 @@ import styles from '../css/LandingPage.css';
 
 function ScheduleLesson() {
     return (
-        <div>
-            <h1>Schedule Lesson</h1>
-            <form className='intro' >
+        <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Sign Up For LEssons Here!</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <form className='intro' >
             <input type="text" placeholder='First Name'/>
             <input type="text" placeholder='Last Name'/>
             <input type="text" placeholder='Email'/>
-            <input type="datetime-local"/>
-            </form>
-        </div>
+            <input type="datetime-local"/></form>
+        </Card.Body>
+      </Card>
     );
-  }
+}
 
 // function updateCoachCalandar
 // function updateClientCalandar
 
 
 export default ScheduleLesson;
+
+{/* <h1>Schedule Lesson</h1>
+<form className='intro' >
+<input type="text" placeholder='First Name'/>
+<input type="text" placeholder='Last Name'/>
+<input type="text" placeholder='Email'/>
+<input type="datetime-local"/></form> */}
