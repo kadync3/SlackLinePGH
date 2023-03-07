30452066af7fdb78cc1a65edfeaefdc42b964f02
kadync3 <106933582+kadync3@users.noreply.github.com>
2023-03-07 10:08:40 -0500

    added google oath

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {createClient } from '@supabase/supabase-js'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
+
+const supabase = createClient(
+  "https://kedyhzhumlqsiiaqqqgq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlZHloemh1bWxxc2lpYXFxcWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxOTg4MDYsImV4cCI6MTk5Mzc3NDgwNn0.xp7wUvusCmHXuePwBzmmGHXyvKK71TqGbmCifRFMj2w"
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <SessionContextProvider supabaseClient={supabase}>
     <App />
+  </SessionContextProvider>
  
 )
